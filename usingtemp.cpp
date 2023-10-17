ffb3739f242f3945a6c6e4986ca2fe8bc1b2d9a3
ShahidMetkari <148204776+ShahidMetkari@users.noreply.github.com>
2023-10-17 03:30:15 -0700

    Create usingtemp.cpp

---
+++ usingtemp.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void swap(int *x, int *y)
+{
+    *x = *x + *y;
+    *y = *x - *y;
+    *x = *x - *y;
+}
+
+int main()
+{
+    int a,b,temp;
+    a=10;
+    b=20;
+
+    cout<<"a:"<<a<<" and b:"<<b<<""<<endl;
+
+    temp = a;
+    a = b;
+    b = temp;
+  cout<<"a:"<<a<<" and b:"<<b<<""<<endl;
+return 0;
+}
